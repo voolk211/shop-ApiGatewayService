73f93e83b8d90d549ce4396c8f9f1f1710963c30
Vyachelav Paltsev <fate6002@gmail.com>
2026-03-01 01:37:24 +0300

    Added JwtTokenFilter

---
+++ src/main/java/org/example/apigatewayservice/jwt/JwtTokenFilter.java
@@ -0,0 +1,87 @@
+package org.example.apigatewayservice.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class JwtTokenFilter implements GlobalFilter, Ordered {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+
+        String path = exchange.getRequest().getPath().value();
+
+        if (path.startsWith("/api/auth/")) {
+            return chain.filter(exchange);
+        }
+
+        String authHeaders = exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+
+        if (authHeaders == null || !authHeaders.startsWith("Bearer ")) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+
+        String token = authHeaders.substring(7);
+
+        try {
+            Claims claims = jwtUtil.validateToken(token);
+
+            Object userId = claims.get("userId");
+
+            if (userId == null) {
+                throw new JwtException("Missing userId");
+            }
+
+            String subject = claims.getSubject();
+
+            if (subject == null) {
+                throw new JwtException("Missing subject");
+            }
+
+            Object rolesObj = claims.get("roles");
+            List<String> roles;
+
+            if (rolesObj instanceof List<?> list) {
+                roles = list.stream()
+                        .map(Object::toString)
+                        .toList();
+            }
+            else {
+                roles = List.of();
+            }
+
+            ServerHttpRequest mutatedRequest = exchange.getRequest()
+                    .mutate()
+                    .header("X-User-Id", userId.toString())
+                    .header("X-Username", subject)
+                    .header("X-Roles", String.join(",", roles))
+                    .build();
+
+            return chain.filter(exchange.mutate().request(mutatedRequest).build());
+        } catch (JwtException e) {
+            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+    }
+}
---
+++ src/main/java/org/example/apigatewayservice/jwt/JwtUtil.java
@@ -0,0 +1,53 @@
+package org.example.apigatewayservice.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import java.time.Instant;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+
+    private final SecretKey secretKey;
+
+    public JwtUtil(
+            @Value("${security.jwt.secret}") String secretKey
+    ) {
+        this.secretKey = Keys.hmacShaKeyFor(secretKey.getBytes());
+    }
+
+    public Claims validateToken(String token) {
+        Claims claims = parseClaims(token);
+        if (!"ACCESS".equals(claims.get("tokenType"))) {
+            throw new JwtException("Not an access token");
+        }
+        validateExpiration(claims);
+        return claims;
+    }
+
+    public Claims parseClaims(String token) {
+        return Jwts.parser()
+                .verifyWith(secretKey)
+                .build()
+                .parseSignedClaims(token)
+                .getPayload();
+    }
+
+    private void validateExpiration(Claims claims) {
+        Date expiration = claims.getExpiration();
+
+        if (expiration == null) {
+            throw new JwtException("Token does not contain expiration");
+        }
+
+        if (expiration.toInstant().isBefore(Instant.now())) {
+            throw new JwtException("Token expired");
+        }
+    }
+}
